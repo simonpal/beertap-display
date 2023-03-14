e4888d7caf47576dd25668072d929d44debb3747
Simon Palmberg <simon.palmberg@gmail.com>
2023-03-14 16:10:41 +0100

    Added manifest, worker and icons

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,7 +1,20 @@
-import * as React from 'react'
-import * as ReactDOM from 'react-dom'
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
 
-import App from './App'
+import App from './App';
 
-const mountNode = document.getElementById('app')
-ReactDOM.render(<App />, mountNode)
+const mountNode = document.getElementById('app');
+ReactDOM.render(<App />, mountNode);
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('/service-worker.js')
+      .then((registration) => {
+        console.log('SW registered: ', registration);
+      })
+      .catch((registrationError) => {
+        console.log('SW registration failed: ', registrationError);
+      });
+  });
+}
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,79 +1,103 @@
-const webpack = require("webpack");
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const Dotenv = require("dotenv-webpack");
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
+const WorkboxPlugin = require('workbox-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 const config = {
-  entry: ["react-hot-loader/patch", "./src/index.tsx"],
+  entry: ['react-hot-loader/patch', './src/index.tsx'],
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "[name].[contenthash].js",
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[contenthash].js',
   },
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
-        use: "babel-loader",
+        use: 'babel-loader',
         exclude: /node_modules/,
       },
       {
         test: /\.ts(x)?$/,
-        loader: "ts-loader",
+        loader: 'ts-loader',
         exclude: /node_modules/,
       },
       {
         test: /\.(jpe?g|png|gif)$/i,
         use: [
           {
-            loader: "url-loader",
+            loader: 'url-loader',
             options: {
-              mimetype: "image/png",
+              mimetype: 'image/png',
             },
           },
         ],
       },
       {
         test: /\.svg$/,
-        use: "file-loader",
+        use: 'file-loader',
       },
     ],
   },
   devServer: {
     static: {
-      directory: "./dist",
+      directory: './dist',
     },
   },
   plugins: [
     new Dotenv(),
     new HtmlWebpackPlugin({
-      title: "Beer display",
+      title: 'Beer display',
       templateContent: ({ htmlWebpackPlugin }) =>
         '<!DOCTYPE html><html><head><meta charset="utf-8"><title>' +
         htmlWebpackPlugin.options.title +
-        "</title>" +
+        '</title>' +
         '<meta name="viewport" content="width=device-width, initial-scale=1" />' +
+        '<link rel="manifest" href="manifest/manifest.json" />' +
         '<link rel="preconnect" href="https://fonts.googleapis.com">' +
         '<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>' +
         '<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;600&display=swap" rel="stylesheet">' +
         '<link href="https://fonts.googleapis.com/css2?family=Righteous&display=swap" rel="stylesheet">' +
+        '<link rel="apple-touch-icon" sizes="180x180" href="images/apple-touch-icon.png">' +
+        '<link rel="icon" type="image/png" sizes="16x16" href="images/favicon-16x16.png">' +
+        '<link rel="apple-touch-icon" sizes="180x180" href="images/apple-touch-icon.png">' +
+        '<meta name="msapplication-TileColor" content="#c6426e">' +
+        '<meta name="theme-color" content="#101018">' +
         '</head><body><div id="app"></div></body></html>',
-      filename: "index.html",
+      filename: 'index.html',
     }),
+    new WorkboxPlugin.GenerateSW({
+      // these options encourage the ServiceWorkers to get in there fast
+      // and not allow any straggling "old" SWs to hang around
+      clientsClaim: true,
+      skipWaiting: true,
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: 'manifest', to: 'manifest' },
+        { from: 'src/images', to: 'images' },
+      ],
+    }),
+    // new CopyWebpackPlugin([
+    //   { from: 'src/assets', to: 'images' },
+    //   { from: 'manifest', to: 'manifest' },
+    // ]),
   ],
   resolve: {
-    extensions: [".tsx", ".ts", ".js"],
+    extensions: ['.tsx', '.ts', '.js'],
     alias: {
-      "react-dom": "@hot-loader/react-dom",
+      'react-dom': '@hot-loader/react-dom',
     },
   },
   optimization: {
-    runtimeChunk: "single",
+    runtimeChunk: 'single',
     splitChunks: {
       cacheGroups: {
         vendor: {
           test: /[\\/]node_modules[\\/]/,
-          name: "vendors",
-          chunks: "all",
+          name: 'vendors',
+          chunks: 'all',
         },
       },
     },
@@ -83,7 +107,7 @@ const config = {
 module.exports = (env, argv) => {
   if (argv.hot) {
     // Cannot use 'contenthash' when hot reloading is enabled.
-    config.output.filename = "[name].[hash].js";
+    config.output.filename = '[name].[hash].js';
   }
 
   return config;
