d2a42063c374f2cc2e127a22590b795c21b6e4ff
Simon Palmberg <simon.palmberg@gmail.com>
2023-03-21 09:01:40 +0100

    Removed server from project

--- server/src/index.ts
+++
@@ -1,9 +0,0 @@
-import app from "./server";
-import * as dotenv from "dotenv";
-import config from "./config";
-
-dotenv.config();
-
-app.listen(config.port, () => {
-  console.log(`App listens on http://localhost:${config.port}`);
-});
